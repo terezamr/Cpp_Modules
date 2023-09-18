aaf305589dcb14a0053b5b55fee2f1e62f71e060
Tereza Melo Ribeiro <80223858+terezamr@users.noreply.github.com>
2023-09-18 16:06:42 +0100

    first

---
+++ megaphone.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+
+int	main(int argc, char **argv)
+{
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+	else
+	{
+		for (int i = 1; i < argc; i++)
+		{
+			std::string str = std::string(argv[i]);
+			for (int f = 0; f < str.size(); f++)
+			{
+				char c = toupper(str[f]);
+				std::cout << c;
+			}
+		}
+		std::cout << "\n";
+	}
+	return (0);
+}
