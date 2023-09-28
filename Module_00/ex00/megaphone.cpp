b1922c07c63c0a10b6d8ac6e6073069c246246cb
Tereza Melo Ribeiro <terezameloribeiro@gmail.com>
2023-09-28 17:10:26 +0100

    28.09

--- Module_00/ex00/megaphone.cpp
+++ Module_00/ex00/megaphone.cpp
@@ -5,7 +5,7 @@
 int	main(int argc, char **argv)
 {
 	if (argc == 1)
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 	else
 	{
 		for (int i = 1; i < argc; i++)
