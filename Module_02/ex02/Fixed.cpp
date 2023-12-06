2b183d7d393722dceb5afb0922e8074fd49ba656
Tereza Melo Ribeiro <terezameloribeiro@gmail.com>
2023-12-06 12:21:20 +0000

    fix

--- Module_02/ex02/Fixed.cpp
+++ Module_02/ex02/Fixed.cpp
@@ -1,6 +1,5 @@
 #include "Fixed.hpp"
 
-//Constructors & Destructor
 Fixed::Fixed()
 {
 	//std::cout << "Default constructor called" << std::endl;
@@ -30,8 +29,6 @@ Fixed::~Fixed()
     //std::cout << "Destructor called" << std::endl;
 }
 
-// 		The copy assignment operator takes a constant reference to
-// 		another object of the same class and copies it
 Fixed& Fixed::operator=(const Fixed& src)
 {
 	//std::cout << "Copy assignment operator called" << std::endl;
@@ -41,7 +38,6 @@ Fixed& Fixed::operator=(const Fixed& src)
 	return *this;
 }
 
-// Get and Set Functions
 int	Fixed::getRawBits(void) const
 {
 	//std::cout << "getRawBits member function called" << std::endl;
@@ -53,20 +49,15 @@ void	Fixed::setRawBits(int const raw)
 	this->FixedPoint = raw;
 }
 
-// 	Converts fixed-point rep. into a floating-point rep.
 float   Fixed::toFloat(void) const
 {
     return (float)(this->getRawBits()) / (1 << Bits);
 }
 
-// 	Converts fixed-point rep. into an integer rep.
 int     Fixed::toInt( void ) const {
     return this->FixedPoint >> Bits;
 }
 
-// Defines how an object of the Fixed class should be formatted when printed
-// By calling the toFloat() method, ensures that the Fixed value is
-// displayed as a floating-point number (used in main)
 std::ostream & operator<<(std::ostream & o, Fixed const & i) {
     o << i.toFloat();
     return o;
