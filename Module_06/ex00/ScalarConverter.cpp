ff3019943ad6144f3d3283970bd73ce210a9abee
Maria Teresa Vicente De Melo Ribeiro <mvicente@c2r3s5.42lisboa.com>
2024-02-08 18:28:06 +0000

    M6 init

---
+++ Module_06/ex00/ScalarConverter.cpp
@@ -0,0 +1,2 @@
+#include "ScalarConverter.hpp"
+
---
+++ Module_06/ex00/ScalarConverter.hpp
@@ -0,0 +1,16 @@
+#ifndef SCALARCONVERTER_HPP
+#define SCALARCONVERTER_HPP
+
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <iomanip>
+
+class ScalarConverter {
+    private:
+
+    public:
+    // no need of an instance in order to use a static method 
+};
+
+#endif
