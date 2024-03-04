d81911d37fc6bb7334a06d35baa5afa8021bfdc8
Tereza Melo Ribeiro <80223858+terezamr@users.noreply.github.com>
2024-03-04 11:43:30 +0000

    04.03 v0

--- Module_09/ex00/BitcoinExchange.cpp
+++ Module_09/ex00/BitcoinExchange.cpp
@@ -10,13 +10,12 @@ BitcoinExchange::BitcoinExchange(std::string str) : input(str)
     // std::cout << "Constructor" << std::endl;
 }
 
-float   ft_stoi(std::string str)
+float   ft_stof(std::string str)
 {
     int f = 0;
     int m = 0;
     float i = 0;
     int d = 1;
-    int point = 0;
     
     if (str[f] == '-')
         throw(std::invalid_argument("Error: negative number"));
@@ -28,9 +27,8 @@ float   ft_stoi(std::string str)
             i = i * 10 + (str[f] - '0');
         else if (str[f] == '.')
         {
-            point++;
             f++;
-            if (!str[f] || !isdigit(str[f]) || point == 2)
+            if (!str[f] || !isdigit(str[f]))
                 throw(std::invalid_argument("Error: invalid rate => " + str));
             while (str[f] >= '0' && str[f] <= '9')
             {
@@ -38,6 +36,8 @@ float   ft_stoi(std::string str)
                 d *= 10;
                 f++;
             }
+            if (str[f] == '.')
+                throw(std::invalid_argument("Error: invalid rate => " + str));
         }
         f++;
     }
@@ -75,48 +75,7 @@ void    BitcoinExchange::populate(std::string str)
         this->mp.insert(split(aux, ','));
     std::map<std::string, std::string>::iterator it;
 
-    // for (it = mp.begin(); it != mp.end(); ++it)
-    //     std::cout << it->first << " => " << it->second << '\n';
-
     f.close();
-}
-
-int check_date(std::string str, int opt, int digits)
-{
-    float f = ft_stoi(str);
-    if (opt == YEAR) {
-        if (digits != 4 || f < 2009 || f > 2024)
-            return 0;
-    }
-    else if (opt == MONTH) {
-        if (digits != 2 || f < 1 || f > 12)
-            return 0;
-    }
-    else if (opt == DAY) {
-        if (digits != 2 || f < 1 || f > 31)
-            return 0;
-    }
-    return 1;
-}
-
-int check_number(std::string str, int opt)
-{
-    std::string s;
-    int i = 0;
-    if (opt == 1)
-        s = str.substr(i, 4);
-    if (opt == 2 || opt == 3)
-        s = str.substr(i, 2);
-    if (!s[0])
-        return 0;
-    int dig = 0;
-    while (str[i] >= '0' && str[i] <= '9') {
-        i++;
-        dig++;
-    }
-    if (!check_date(s, opt, dig))
-        return 0;
-    return 1;
 }
 
 float    check_rate(std::string str)
@@ -135,10 +94,71 @@ float    check_rate(std::string str)
         dig++;
     }
     s = str.substr(i - dig, dig);
-    float f = ft_stoi(s);
+    float f = ft_stof(s);
     return f;
 }
 
+// checks number of digits and if all chars are digits
+int check_number(std::string s, int opt)
+{
+    int i = 0;
+    while (s[i])
+    {
+        if (s[i] < '0' || s[i] > '9')
+            return 0;
+        i++;
+    }
+    if (i != opt)
+        return 0;
+    return 1;
+}
+
+// checks if the date exists
+int check_ymd(std::string y, std::string m, std::string d)
+{
+    if (!y[0] || !m[0] || !d[0])
+        return 0;
+
+    if (!check_number(y, 4) || !check_number(m, 2) || !check_number(d, 2))
+        return 0;
+
+    int y1 = static_cast<int>(ft_stof(y));
+    int m1 = static_cast<int>(ft_stof(m));
+    int d1 = static_cast<int>(ft_stof(d));
+
+    int dd[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    if (y1 % 4 == 0)
+        dd[1] = 29;
+
+    if (m1 < 1 || m1 > 12 || d1 < 1 || d1 > dd[m1 - 1])
+        return 0;
+    return 1;
+}
+
+void    check_dates(std::string s1)
+{
+    size_t pos = s1.find('-');
+    if (pos == std::string::npos)
+        throw(std::invalid_argument("Error: invalid input => " + s1));
+
+    pos = s1.substr(pos + 1).find('-');
+    if (pos == std::string::npos)
+        throw(std::invalid_argument("Error: invalid input => " + s1));
+
+    std::string aux = split(s1, '-').second;
+
+    std::string y = split(s1, '-').first;
+    std::string m = split(aux, '-').first;
+    std::string d = split(aux, '-').second;
+
+    if (!check_ymd(y, m, d))
+        throw(std::invalid_argument("Error: invalid input => " + s1));
+
+    if (s1 < "2009-01-02")
+        throw(std::invalid_argument("Error: invalid input => " + s1));
+}
+
+// trims whitespaces and checks the necessary spaces (before and after '|')
 std::string ft_trim(std::string str, int opt)
 {
     std::string final;
@@ -158,26 +178,6 @@ std::string ft_trim(std::string str, int opt)
     return final;
 }
 
-void    check_dates(std::string s1)
-{
-    size_t pos = s1.find('-');
-    if (pos == std::string::npos)
-        throw(std::invalid_argument("Error: invalid input => " + s1));
-
-    pos = s1.substr(pos + 1).find('-');
-    if (pos == std::string::npos)
-        throw(std::invalid_argument("Error: invalid input => " + s1));
-
-    std::string aux = split(s1, '-').second;
-
-    std::string d1 = split(s1, '-').first;
-    std::string d2 = split(aux, '-').first;
-    std::string d3 = split(aux, '-').second;
-
-    if (!check_number(d1, YEAR) || !check_number(d2, MONTH) || !check_number(d3, DAY))
-        throw(std::invalid_argument("Error: invalid input => " + s1));
-}
-
 std::pair<std::string, float> check_line(std::string str)
 {
     if (!str[0])
@@ -185,7 +185,7 @@ std::pair<std::string, float> check_line(std::string str)
 
     size_t pos = str.find('|');
     if (pos == std::string::npos)
-        throw(std::invalid_argument("Error: invalid input"));
+        throw(std::invalid_argument("Error: invalid input => " + str));
 
     std::string s1 = ft_trim(str.substr(0, pos), 1);
     std::string s2 = ft_trim(str.substr(pos + 1, str.size()), 2);
@@ -201,10 +201,10 @@ std::pair<std::string, float> check_line(std::string str)
 float  BitcoinExchange::find_date(std::string date)
 {
     std::map<std::string, std::string>::iterator it = this->mp.begin();
-            (void)it;
-            (void)date;
-
-    return 5;
+    while (it != mp.end() && it->first <= date)
+        it++;
+    it--;
+    return (ft_stof(it->second));
 }
 
 void    BitcoinExchange::showValues(void)
@@ -216,24 +216,24 @@ void    BitcoinExchange::showValues(void)
     f.open(this->input.c_str(), std::ios::in);
     std::string aux;
     getline(f, aux);
+
     if (!aux[0]) {
         std::cout << "Error: empty file" << std::endl;
         return ;
     }
     if (aux != "date | value") {
-        std::cout << "Error: invalid file format" << std::endl;
+        std::cout << "Error: invalid file format => " << aux << std::endl;
         return ;
     }
+
     int i = 1;
     while (getline(f, aux)) {
         try {
             std::pair<std::string, float> pr = check_line(aux);
-            std::cout << i << ". " << pr.first << " | " << pr.second << std::endl;
-
-            // encontrar linha mais perto da db 1 (precisamos da data da linha)
+            std::cout << pr.first << " => " << pr.second;
+ 
             float closest_value = this->find_date(pr.first);
-            // fazer conta (precisamos do rate da linha)
-            (void)closest_value;
+            std::cout << " = " << closest_value * pr.second << std::endl;
         }
         catch(const std::invalid_argument& e) {
             std::cerr << e.what() << '\n';
